8f82ece54a944920bfdcc1a879b5bc89b609d65b
SunilRSI <150338544+sunilRSI@users.noreply.github.com>
2024-01-09 18:32:00 +0530

    SG_Feature_Branch_Testcases "Testcases" (#6)
    * SG_Feature_Branch_Testcases "Testcases"
    
    * SG_Feature_Branch_Testcases undo appsettings.json
    
    * SG_Feature_Branch_Testcases undo changes
    
    ---------
    
    Co-authored-by: Sunil Ghutukade <Sunil.Ghutukade@grassvalley.com>

---
+++ OrganizationSolution/Source/Test/Geography.ServerlessTests/Models/UpdateCountryResponse.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Geography.ServerlessTests.Models
+{
+    public class UpdateCountryResponse
+    {
+        public UpdateCountry updateCountry { get; set; }
+    }
+
+    public class UpdateCountry
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string isoCode { get; set; }
+        public string continent { get; set; }
+    }
+}
