04c24dbe3bc769d636dd39433b233b2d0d0a6ee2
Sunil Ghutukade <Sunil.Ghutukade@grassvalley.com>
2024-01-09 13:43:51 +0530

    RSITestcase initial commit

---
+++ OrganizationSolution/Source/App/Geography.ServerlessTests/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Framework.Business.ServiceProvider.Storage;
+using Framework.Configuration.Models;
+using Geography.Business;
+using Geography.Serverless.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Geography.ServerlessTests
+{
+    /// <summary>
+    /// Defines the <see cref="Startup" />.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration<see cref="IConfiguration"/>.</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the Configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// The ConfigureServices.
+        /// </summary>
+        /// <param name="services">The services<see cref="IServiceCollection"/>.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddControllersWithViews().AddNewtonsoftJson();
+            services.AddScoped(typeof(IStorageManager<AmazonS3ConfigurationOptions>), typeof(StorageManager));
+            services.ConfigureClientServices();
+            services.ConfigureGraphQLServices();
+            services.ConfigureDataProvider();
+            services.ConfigureAwsCongnitoSecurity();
+        }
+        /// <summary>
+        /// The Configure.
+        /// </summary>
+        /// <param name="app">The app<see cref="IApplicationBuilder"/>.</param>
+        /// <param name="env">The env<see cref="IWebHostEnvironment"/>.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
---
+++ OrganizationSolution/Source/App/Geography.ServerlessTests/TestSetup.cs
@@ -0,0 +1,43 @@
+﻿using Framework.Configuration;
+using Framework.Configuration.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Geography.ServerlessTests
+{
+    /// <summary>
+    /// Defines the <see cref="TestSetup" />.
+    /// </summary>
+    public class TestSetup<T>
+    {
+        /// <summary>
+        /// The Main.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        public async Task<T> Main(string[] args)
+        {
+            T obj;
+
+            var host = CreateHostBuilder(args).Build();
+
+            var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            obj = services.GetRequiredService<T>();
+            return obj;  
+        }
+
+        /// <summary>
+        /// The CreateHostBuilder.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        /// <returns>The <see cref="IHostBuilder"/>.</returns> 
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .DefaultAppConfiguration(new[] { typeof(ApplicationOptions).Assembly }, args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
